12ff45edc1c1de081069f81d90e1bc6c45a893af
uct-trk <ugurcan.uut82@gmail.com>
2024-01-01 23:17:15 +0300

    cart items count on header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,47 @@
-import {Navbar, Nav, Container} from 'react-bootstrap'
-import {FaShoppingCart, FaUser} from 'react-icons/fa'
-import logo from '../assets/logo.png'
-import {LinkContainer} from 'react-router-bootstrap'    
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <>
-        <header>
-            <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
-                <Container>
-                    <LinkContainer to="/">
-                        <Navbar.Brand><img src={logo} alt="" />UctShop</Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                        <LinkContainer to="/cart">
-                            <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
-                                </LinkContainer>
-                            <LinkContainer to="/login">
-                                <Nav.Link><FaUser/> User</Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <img src={logo} alt="" />
+                UctShop
+              </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <LinkContainer to="/cart">
+                  <Nav.Link>
+                    <FaShoppingCart /> Cart
+                    {cartItems.length > 0 && (
+                      <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                        {cartItems.reduce((acc, cur) => acc + cur.qty, 0)}
+                      </Badge>
+                    )}
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>
+                    <FaUser /> User
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
