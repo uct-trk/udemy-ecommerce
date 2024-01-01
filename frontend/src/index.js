9c875c05a13f726f794c898bd6d153f9442db794
uct-trk <ugurcan.uut82@gmail.com>
2024-01-01 18:35:34 +0300

    redux toolkit installed and created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import React from "react";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
-      { 
-        path: '/',
+      {
+        path: "/",
         element: <HomeScreen />,
       },
-      { 
-        path: '/product/:id',
+      {
+        path: "/product/:id",
         element: <ProductScreen />,
-      }
-    ]
-  } 
-])
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
