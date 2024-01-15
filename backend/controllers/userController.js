000ed7cc1bd2e537fe3137e77dad7b173ba3ac2e
uct-trk <ugurcan.uut82@gmail.com>
2024-01-15 23:42:56 +0300

    register & encyription

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-import jwt from "jsonwebtoken";
+import generateToken from "../utils/generateToken.js";
 
 // @desc Auth user & get token
 // @route post / api/users/login
@@ -11,17 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    // set jwt http-only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development", // Use secure cookies in 
-      sameSite: "strict", // Prevent CSRF attacks
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -40,18 +30,45 @@ const authUser = asyncHandler(async (req, res) => {
 // @route post / api/users
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc logout user / clear cookie
 // @route post / api/users/logout
 // @access private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.cookie('jwt', '', {
-    httpOnly:true,
-    expires: new Date(0)
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
   });
-  res.status(200).json({message: 'Logged out successfully'})
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc get user profile
