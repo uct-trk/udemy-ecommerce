f0e78f337c37b6b6c533644024b999bb9265d1ab
uct-trk <ugurcan.uut82@gmail.com>
2024-01-15 23:20:44 +0300

    logout user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -47,7 +47,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route post / api/users/logout
 // @access private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie('jwt', '', {
+    httpOnly:true,
+    expires: new Date(0)
+  });
+  res.status(200).json({message: 'Logged out successfully'})
 });
 
 // @desc get user profile
