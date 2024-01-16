22ad462cce2fc547b55628d05f5cfc2115bb7730
uct-trk <ugurcan.uut82@gmail.com>
2024-01-16 22:42:03 +0300

    users api slice & auth

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './slices/apiSlice.js'
 import cartSliceReducer from './slices/cartSlice.js'
+import authSliceReducer from './slices/authSlice.js'
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]:apiSlice.reducer,
-    cart:cartSliceReducer
+    cart:cartSliceReducer,
+    auth:authSliceReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools:true
