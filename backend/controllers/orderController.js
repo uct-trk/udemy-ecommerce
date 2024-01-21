44b093c36ec71ef0835499b61e4236512415d312
uct-trk <ugurcan.uut82@gmail.com>
2024-01-21 15:37:24 +0300

    create and get order backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,63 @@ import Order from "../models/orderModel.js";
 // @route Post / api/orders
 // @access private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((order) => ({
+        ...order,
+        product: order._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc get logged in user orders
 // @route get / api/orders/myorders
 // @access private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const order = await Order.find({ user: req.user._id }).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc get order by id
 // @route get / api/orders/:id
 // @access private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id);
+  res.status(200).json(order);
 });
 
 // @desc update Order to paid
